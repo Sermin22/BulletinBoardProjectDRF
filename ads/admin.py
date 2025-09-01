685c09a98c33637fb773fa2a36ca2c0cdc2c4f23
Sergey Minin <sm.minins@gmail.com>
2025-09-01 12:04:16 +0300

    add class CommentAdmin

--- ads/admin.py
+++ ads/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ads.models import Advertisement
+from ads.models import Advertisement, Comment
 
 
 @admin.register(Advertisement)
@@ -7,3 +7,10 @@ class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "price", "description", "author", "created_at")
     list_filter = ("title", "author", "created_at")
     search_fields = ("title", "author")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "author_id", "author", "advertisement_id", "advertisement", "created_at")
+    list_filter = ("text", "author", "created_at")
+    search_fields = ("text", "author")
